386bebd22c0c8f0122bf3b17dac9605253a965e6
Iris van Willigen <iris.van.willigen@gmail.com>
2025-09-15 13:39:04 +0200

    #1 add css reset to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
 </script>
@@ -9,3 +9,20 @@
 </svelte:head>
 
 {@render children?.()}
+
+<style>
+	/* Reset */
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	button,
+	input,
+	select,
+	textarea {
+		font-family: inherit;
+		font-size: 100%;
+	}
+</style>
