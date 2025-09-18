9f73444bdc49d9712de4e2b9187b4ec47b2b62e7
Iris van Willigen <iris.van.willigen@gmail.com>
2025-09-18 10:26:29 +0200

    add appDir and paths configuration to svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		appDir: 'app',
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
@@ -11,7 +12,10 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: false
-		})
+		}),
+		paths: {
+            base: dev ? '' : process.env.BASE_PATH,
+        }
 	}
 };
 
