ac91aaa0e04a23d4bdd4ceeb92e6a1284589b62e
Iris van Willigen <iris.van.willigen@gmail.com>
2025-09-19 10:27:38 +0200

    update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -5,16 +5,14 @@ const config = {
 	kit: {
 		appDir: 'app',
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: 'index.html', // Enable client-side routing for deep links!
 			precompress: false,
 			strict: false
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.BASE_PATH || ''
 		}
 	}
 };
